149bc9268481ba10d211f0c3b31c86aaea074127
Yablonya <homyakk3@gmail.com>
2022-10-09 14:07:33 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace MS_lr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QueueAsspirantTimeProgressBar = new System.Windows.Forms.ProgressBar();
+            this.AsspirantLabel = new System.Windows.Forms.Label();
+            this.AsspirantAskingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.QueueTeacherProgressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TeacherAskingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.studentsProgressBar = new System.Windows.Forms.ProgressBar();
+            this.speedSystemScrollBar = new System.Windows.Forms.HScrollBar();
+            this.AsspirantAskingTimeProgressBar = new System.Windows.Forms.ProgressBar();
+            this.TeacherAskingTimeProgressBar = new System.Windows.Forms.ProgressBar();
+            this.QueueAsspirantProgressBar = new System.Windows.Forms.ProgressBar();
+            this.studentsCountLabel = new System.Windows.Forms.Label();
+            this.asspirantCountLabel = new System.Windows.Forms.Label();
+            this.teacherCountLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // QueueAsspirantTimeProgressBar
+            // 
+            this.QueueAsspirantTimeProgressBar.Location = new System.Drawing.Point(111, 25);
+            this.QueueAsspirantTimeProgressBar.Name = "QueueAsspirantTimeProgressBar";
+            this.QueueAsspirantTimeProgressBar.Size = new System.Drawing.Size(100, 23);
+            this.QueueAsspirantTimeProgressBar.TabIndex = 0;
+            // 
+            // AsspirantLabel
+            // 
+            this.AsspirantLabel.AutoSize = true;
+            this.AsspirantLabel.Location = new System.Drawing.Point(351, 77);
+            this.AsspirantLabel.Name = "AsspirantLabel";
+            this.AsspirantLabel.Size = new System.Drawing.Size(46, 17);
+            this.AsspirantLabel.TabIndex = 1;
+            this.AsspirantLabel.Text = "label1";
+            // 
+            // AsspirantAskingProgressBar
+            // 
+            this.AsspirantAskingProgressBar.Location = new System.Drawing.Point(325, 12);
+            this.AsspirantAskingProgressBar.Name = "AsspirantAskingProgressBar";
+            this.AsspirantAskingProgressBar.Size = new System.Drawing.Size(100, 22);
+            this.AsspirantAskingProgressBar.TabIndex = 2;
+            // 
+            // QueueTeacherProgressBar
+            // 
+            this.QueueTeacherProgressBar.Location = new System.Drawing.Point(431, 25);
+            this.QueueTeacherProgressBar.Name = "QueueTeacherProgressBar";
+            this.QueueTeacherProgressBar.Size = new System.Drawing.Size(100, 22);
+            this.QueueTeacherProgressBar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(563, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // TeacherAskingProgressBar
+            // 
+            this.TeacherAskingProgressBar.Location = new System.Drawing.Point(537, 12);
+            this.TeacherAskingProgressBar.Name = "TeacherAskingProgressBar";
+            this.TeacherAskingProgressBar.Size = new System.Drawing.Size(100, 22);
+            this.TeacherAskingProgressBar.TabIndex = 5;
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(12, 54);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(75, 23);
+            this.RunButton.TabIndex = 6;
+            this.RunButton.Text = "button1";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // studentsProgressBar
+            // 
+            this.studentsProgressBar.Location = new System.Drawing.Point(5, 26);
+            this.studentsProgressBar.Name = "studentsProgressBar";
+            this.studentsProgressBar.Size = new System.Drawing.Size(100, 22);
+            this.studentsProgressBar.TabIndex = 7;
+            // 
+            // speedSystemScrollBar
+            // 
+            this.speedSystemScrollBar.Location = new System.Drawing.Point(111, 56);
+            this.speedSystemScrollBar.Name = "speedSystemScrollBar";
+            this.speedSystemScrollBar.Size = new System.Drawing.Size(100, 21);
+            this.speedSystemScrollBar.TabIndex = 8;
+            // 
+            // AsspirantAskingTimeProgressBar
+            // 
+            this.AsspirantAskingTimeProgressBar.Location = new System.Drawing.Point(325, 40);
+            this.AsspirantAskingTimeProgressBar.Name = "AsspirantAskingTimeProgressBar";
+            this.AsspirantAskingTimeProgressBar.Size = new System.Drawing.Size(100, 22);
+            this.AsspirantAskingTimeProgressBar.TabIndex = 9;
+            // 
+            // TeacherAskingTimeProgressBar
+            // 
+            this.TeacherAskingTimeProgressBar.Location = new System.Drawing.Point(537, 40);
+            this.TeacherAskingTimeProgressBar.Name = "TeacherAskingTimeProgressBar";
+            this.TeacherAskingTimeProgressBar.Size = new System.Drawing.Size(100, 22);
+            this.TeacherAskingTimeProgressBar.TabIndex = 10;
+            // 
+            // QueueAsspirantProgressBar
+            // 
+            this.QueueAsspirantProgressBar.Location = new System.Drawing.Point(217, 25);
+            this.QueueAsspirantProgressBar.Name = "QueueAsspirantProgressBar";
+            this.QueueAsspirantProgressBar.Size = new System.Drawing.Size(100, 23);
+            this.QueueAsspirantProgressBar.TabIndex = 11;
+            // 
+            // studentsCountLabel
+            // 
+            this.studentsCountLabel.AutoSize = true;
+            this.studentsCountLabel.Location = new System.Drawing.Point(146, 174);
+            this.studentsCountLabel.Name = "studentsCountLabel";
+            this.studentsCountLabel.Size = new System.Drawing.Size(46, 17);
+            this.studentsCountLabel.TabIndex = 12;
+            this.studentsCountLabel.Text = "label1";
+            // 
+            // asspirantCountLabel
+            // 
+            this.asspirantCountLabel.AutoSize = true;
+            this.asspirantCountLabel.Location = new System.Drawing.Point(198, 174);
+            this.asspirantCountLabel.Name = "asspirantCountLabel";
+            this.asspirantCountLabel.Size = new System.Drawing.Size(46, 17);
+            this.asspirantCountLabel.TabIndex = 13;
+            this.asspirantCountLabel.Text = "label1";
+            // 
+            // teacherCountLabel
+            // 
+            this.teacherCountLabel.AutoSize = true;
+            this.teacherCountLabel.Location = new System.Drawing.Point(250, 174);
+            this.teacherCountLabel.Name = "teacherCountLabel";
+            this.teacherCountLabel.Size = new System.Drawing.Size(46, 17);
+            this.teacherCountLabel.TabIndex = 14;
+            this.teacherCountLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.teacherCountLabel);
+            this.Controls.Add(this.asspirantCountLabel);
+            this.Controls.Add(this.studentsCountLabel);
+            this.Controls.Add(this.QueueAsspirantProgressBar);
+            this.Controls.Add(this.TeacherAskingTimeProgressBar);
+            this.Controls.Add(this.AsspirantAskingTimeProgressBar);
+            this.Controls.Add(this.speedSystemScrollBar);
+            this.Controls.Add(this.studentsProgressBar);
+            this.Controls.Add(this.RunButton);
+            this.Controls.Add(this.TeacherAskingProgressBar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.QueueTeacherProgressBar);
+            this.Controls.Add(this.AsspirantAskingProgressBar);
+            this.Controls.Add(this.AsspirantLabel);
+            this.Controls.Add(this.QueueAsspirantTimeProgressBar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar QueueAsspirantTimeProgressBar;
+        private System.Windows.Forms.Label AsspirantLabel;
+        private System.Windows.Forms.ProgressBar AsspirantAskingProgressBar;
+        private System.Windows.Forms.ProgressBar QueueTeacherProgressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar TeacherAskingProgressBar;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.ProgressBar studentsProgressBar;
+        private System.Windows.Forms.HScrollBar speedSystemScrollBar;
+        private System.Windows.Forms.ProgressBar AsspirantAskingTimeProgressBar;
+        private System.Windows.Forms.ProgressBar TeacherAskingTimeProgressBar;
+        private System.Windows.Forms.ProgressBar QueueAsspirantProgressBar;
+        private System.Windows.Forms.Label studentsCountLabel;
+        private System.Windows.Forms.Label asspirantCountLabel;
+        private System.Windows.Forms.Label teacherCountLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MS_lr
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> studentsList;
+        private List<Student> asspirantsStudentsList;
+        private List<Student> teacherStudentsList;
+        private int M;
+        private int speed;
+        private int t0;
+        private int addToAsspirantTime;
+        private int time;
+
+        private int n1;
+        private int ask_asspirant;
+        private int ask_true_asspirant;
+        private int t_asspirant;
+        private int t1;
+
+        private int n2;
+        private int ask_teacher;
+        private int ask_true_teacher;
+        private int t_teacher;
+        private int t2;
+
+        private int redirect_to_students;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addToAsspirantList() 
+        {
+            if (addToAsspirantTime / t0 > 0)
+            {
+                if (studentsList.Count != 0)
+                {
+                    addToAsspirantTime = addToAsspirantTime - t0;
+                    asspirantsStudentsList.Add(new Student());
+                    studentsList.RemoveAt(0);
+                    studentsCountLabel.Text = studentsList.Count.ToString();
+                    studentsProgressBar.Value = studentsList.Count;
+                    QueueAsspirantProgressBar.Value = asspirantsStudentsList.Count;
+                }
+            }
+            else 
+            {
+                addToAsspirantTime++;
+                QueueAsspirantTimeProgressBar.Value = addToAsspirantTime;
+            }
+        }
+        private void askingAsspirant() 
+        {
+            if (ask_asspirant <= n1)
+            {
+                AsspirantAskingProgressBar.Value = ask_asspirant;
+                if (t_asspirant / t1 > 0)
+                {
+                    ask_asspirant++;
+                    t_asspirant = t_asspirant - t1;
+                    Random rnd = new Random();
+                    int value = rnd.Next(0, 1);
+                    if (value == 1) ask_true_asspirant++;
+                }
+                else
+                {
+                    t_asspirant++;
+                    AsspirantAskingTimeProgressBar.Value = t_asspirant;
+                }
+            }
+            else 
+            {
+                if (!((float)ask_true_asspirant / n1 >= 0.3 && (float)ask_true_asspirant / n1 <= 0.5))
+                {
+                    teacherStudentsList.Add(new Student());
+                    teacherCountLabel.Text = teacherStudentsList.Count.ToString();
+                }
+
+                asspirantsStudentsList.RemoveAt(0);
+                asspirantCountLabel.Text = asspirantsStudentsList.Count.ToString();
+                ask_asspirant = 0;
+                ask_true_asspirant = 0;
+            }
+        }
+        private void askingTeacher()
+        {
+            if (ask_teacher <= n2)
+            {
+                if (t_teacher / t2 < 0)
+                    t_teacher++;
+                else
+                {
+                    ask_teacher++;
+                    t_teacher = t_teacher - t2;
+                    Random rnd = new Random();
+                    int value = rnd.Next(0, 1);
+                    if (value == 1) ask_true_teacher++;
+                }
+            }
+            else
+            {
+                if ((float)ask_true_teacher / n2 <= 0.2) {
+                    if (redirect_to_students / 2 * t0 < 0)
+                        redirect_to_students++;
+                    else
+                    {
+                        redirect_to_students = redirect_to_students - 2 * t0;
+                        studentsList.Add(new Student());
+                        studentsCountLabel.Text = studentsList.Count.ToString();
+                    }
+                }
+                else
+                {
+                    teacherStudentsList.RemoveAt(0);
+                    teacherCountLabel.Text = teacherStudentsList.Count.ToString();
+                    ask_teacher = 0;
+                    ask_true_teacher = 0;
+                }
+
+
+            }
+        }
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            asspirantsStudentsList = new List<Student>();
+            teacherStudentsList = new List<Student>();
+
+            M = 15;
+            studentsList = new List<Student>(M);
+            for (int i = 0; i < M; i++)
+                studentsList.Add(new Student());
+
+            studentsProgressBar.Minimum = 0;
+            studentsProgressBar.Maximum = M;
+            studentsProgressBar.Value = studentsList.Count;
+            studentsProgressBar.Refresh();
+
+            
+            speedSystemScrollBar.Minimum = 100;
+            speedSystemScrollBar.Maximum = 3000;
+            speedSystemScrollBar.Value = 1000;
+
+            t0 = 600;
+            addToAsspirantTime = 0;
+
+            n1 = 5;
+            t1 = 300;
+            ask_asspirant = 0;
+            t_asspirant = 0;
+            ask_true_asspirant = 0;
+
+            n2 = 5;
+            t2 = 420;
+            ask_teacher = 0;
+            t_teacher = 0;
+            ask_true_teacher = 0;
+            redirect_to_students = 0;
+
+
+            QueueAsspirantTimeProgressBar.Minimum = 0;
+            QueueAsspirantTimeProgressBar.Maximum = t0;
+
+            QueueAsspirantProgressBar.Minimum = 0;
+            QueueAsspirantProgressBar.Maximum = M;
+
+            AsspirantAskingProgressBar.Minimum = 0;
+            AsspirantAskingProgressBar.Maximum = n1;
+            AsspirantAskingTimeProgressBar.Minimum = 0;
+            AsspirantAskingTimeProgressBar.Maximum = t1;
+
+            speed = speedSystemScrollBar.Value;
+
+            while (studentsList.Count != 0 || asspirantsStudentsList.Count != 0 || teacherStudentsList.Count != 0) 
+            {
+
+                addToAsspirantList();
+
+                if (asspirantsStudentsList.Count > 0)
+                    askingAsspirant();
+
+                if (teacherStudentsList.Count > 0)
+                    askingTeacher();
+
+                time++;
+                studentsProgressBar.Value = studentsList.Count;
+                if (speed != speedSystemScrollBar.Value)
+                    speed = speedSystemScrollBar.Value;
+                Thread.Sleep(1);
+                        
+            }
+
+        }
+    }
+}
+
